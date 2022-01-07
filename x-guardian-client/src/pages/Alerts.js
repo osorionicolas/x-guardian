32557c016126d7e9ebe54746c3c878bd493b6356
Nicolás Osorio <osorionicolas95@gmail.com>
2022-01-06 22:26:03 -0300

    Get location and alert types, added create alert function

--- x-guardian-client/src/pages/Alerts.js
+++ x-guardian-client/src/pages/Alerts.js
@@ -1,48 +1,51 @@
-import React, { useEffect } from 'react'
-import { Icon, Box, Grid } from '@chakra-ui/react'
-import { MdOutlinePets } from 'react-icons/md'
-import { GiRobber } from 'react-icons/gi'
-import { RiFloodFill } from 'react-icons/ri'
-import { BsLightbulbOffFill } from 'react-icons/bs'
+import React, { useEffect, useState } from 'react'
+import { Box, Grid } from '@chakra-ui/react'
+
 import Card from '../components/Card'
+import axios from 'axios'
+import { API_URL } from '../environment'
 
 export const Alerts = () => {
+    const [alerts, setAlerts] = useState([])
+    const [coords, setCoords] = useState()
 
     const fetchData = async () => {
-        /*try {
-            
-            axios(`${API_URL}/recommendations`).then(response => {
-                    const data = response.data
-                    const keys = Object.keys(data)
-                    const randomObject = Math.floor(Math.random()*keys.length)
-                    const courses = data[keys[randomObject]]
-                    setRecommendations(courses)
-                })
-
-                axios(`${API_URL}/topics`).then(topicsResponse => setTopics(topicsResponse.data))
-                axios(`${API_URL}/users/interests`).then(interestsResponse => setInsterests(interestsResponse.data))
-            }
-            else {
-                axios(`${API_URL}/visitors/recommendations`).then(response => setRecommendations(response.data.discover))
-            }
-            
+        try {
+            axios(`${API_URL}/alert-types`).then(response => {
+                const data = response.data
+                console.log(data)
+                setAlerts(data)
+            })
         }
         catch(error){
             console.log(error)          
-        }*/
+        }
+    }
+
+    const getLocation = () => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const coords = (({ latitude, longitude }) => ({ latitude, longitude }))(position.coords)
+                console.log(coords)
+                setCoords(coords)
+            },
+            (error) => {
+                console.error("Error Code = " + error.code + " - " + error.message)
+            }
+        )
     }
     
     useEffect(() => {
         fetchData()
+        getLocation()
     }, [])
 
     return (
         <Box display='flex' height='calc(100vh - 80px)' justifyContent='center' alignItems='center' flexWrap='wrap'>
             <Grid gridTemplateColumns='repeat(auto-fit, minmax(20rem, 1fr))' w='100%' gap='4rem' p='4rem'>
-                <Card title="Crime" icon={<Icon as={GiRobber} boxSize={28}/>}></Card>
-                <Card title="Stray" icon={<Icon as={MdOutlinePets} boxSize={28}/>}></Card>
-                <Card title="Flood" icon={<Icon as={RiFloodFill} boxSize={28}/>}></Card>
-                <Card title="Outage" icon={<Icon as={BsLightbulbOffFill} boxSize={28}/>}></Card>
+                {alerts.map(alert => {
+                    return <Card alert={alert} coords={coords} key={alert.id}></Card>
+                })}
             </Grid>
         </Box>
     )
